b74b2fd7d5e1bb219ca29a5e93eeca26d9f428b8
Mubbly <melissa.eenmaa@itcollege.ee>
2016-07-08 23:31:00 +0300

    started writing tests

---
+++ main.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+
+class BinaryTime:
+
+    def __init__(self, dt: datetime):
+        self.hour_tens = []
+        self.hour_ones = []
+        self.minute_tens = []
+        self.minute_ones = []
+        self.second_tens = []
+        self.second_ones = []
+
+
+class Display:
+    def output_time(self):
+        pass
+
+
+class BinaryClock:
+    def run(self):
+        pass
+
--- tests.py
+++ tests.py
@@ -1,10 +1,24 @@
 import unittest
+from main import BinaryTime
+from datetime import datetime
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
 
+
+class TestBinaryTime(unittest.TestCase):
+    def test(self):
+        time1 = datetime(hour=0, minute=0, second=0)
+        time2 = datetime(hour=20, minute=30, second=45)
+
+        bin_time1 = BinaryTime(time1)
+
+        self.assertEqual(
+            bin_time1.hour_ones,
+            [0, 0, 0, 0]
+        )
 
 if __name__ == '__main__':
     unittest.main()
