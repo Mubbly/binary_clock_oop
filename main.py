cb3470156ff2483293ee8a905aea575dcb7927a1
Mubbly <melissa.eenmaa@itcollege.ee>
2016-07-09 15:32:08 +0300

    complete

--- main.py
+++ main.py
@@ -1,23 +1,75 @@
 from datetime import datetime
+from time import sleep
 
 
 class BinaryTime:
+    @staticmethod
+    def int_to_bin_list(i: int) -> list:
+        bin_time = [0, 0, 0, 0]
+        if i >= 8:
+            bin_time[3] = 1
+            i -= 8
+        if i >= 4:
+            bin_time[2] = 1
+            i -= 4
+        if i >= 2:
+            bin_time[1] = 1
+            i -= 2
+        if i >= 1:
+            bin_time[0] = 1
+        return bin_time
 
-    def __init__(self, dt: datetime):
-        self.hour_tens = []
-        self.hour_ones = []
-        self.minute_tens = []
-        self.minute_ones = []
-        self.second_tens = []
-        self.second_ones = []
+    def __init__(self, h: int, m: int, s: int):
+        #                [1, 2, 4, 8]
+        self.hour_tens = self.int_to_bin_list(h / 10)
+        self.hour_ones = self.int_to_bin_list(h % 10)
+        self.minute_tens = self.int_to_bin_list(m / 10)
+        self.minute_ones = self.int_to_bin_list(m % 10)
+        self.second_tens = self.int_to_bin_list(s / 10)
+        self.second_ones = self.int_to_bin_list(s % 10)
 
 
 class Display:
-    def output_time(self):
-        pass
+    @staticmethod
+    def bin_time_to_str(bin_time: BinaryTime) -> str:
+        bin_str = ""
+        bin_str_tmpl = \
+            "[{ht}][{ho}][{mt}][{mo}][{st}][{so}]\n"
+
+        ht = bin_time.hour_tens
+        ho = bin_time.hour_ones
+        mt = bin_time.minute_tens
+        mo = bin_time.minute_ones
+        st = bin_time.second_tens
+        so = bin_time.second_ones
+
+        for i in range(3, -1, -1):
+            bin_str += bin_str_tmpl.format(
+                ht=ht[i],
+                ho=ho[i],
+                mt=mt[i],
+                mo=mo[i],
+                st=st[i],
+                so=so[i]
+            )
+        return bin_str \
+            .replace("0", " ") \
+            .replace("1", "X")
 
 
 class BinaryClock:
     def run(self):
-        pass
+        while True:
+            # get current time
+            current_time = datetime.now()
+            # convert to bin time
+            current_time_bin = BinaryTime(current_time.hour, current_time.minute, current_time.second)
+            # display bin time
+            current_time_str = Display.bin_time_to_str(current_time_bin)
+            print(current_time_str)
+            sleep(1)
 
+
+if __name__ == '__main__':
+    bin_clock = BinaryClock()
+    bin_clock.run()
--- tests.py
+++ tests.py
@@ -1,23 +1,88 @@
 import unittest
-from main import BinaryTime
-from datetime import datetime
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+from main import BinaryTime, Display
 
 
 class TestBinaryTime(unittest.TestCase):
-    def test(self):
-        time1 = datetime(hour=0, minute=0, second=0)
-        time2 = datetime(hour=20, minute=30, second=45)
+    @staticmethod
+    def bin_time_to_matrix(bin_time: BinaryTime) -> list:
+        return [
+            bin_time.hour_tens,
+            bin_time.hour_ones,
+            bin_time.minute_tens,
+            bin_time.minute_ones,
+            bin_time.second_tens,
+            bin_time.second_ones
+        ]
 
-        bin_time1 = BinaryTime(time1)
+    def test(self):
+        bin_time1 = BinaryTime(0, 0, 0)
+        bin_time2 = BinaryTime(20, 30, 45)
+        bin_time3 = BinaryTime(12, 12, 30)
+
+        bin_time1_mat = self.bin_time_to_matrix(bin_time1)
+        bin_time2_mat = self.bin_time_to_matrix(bin_time2)
+        bin_time3_mat = self.bin_time_to_matrix(bin_time3)
+
+        #    1  2  4  8
+        expected1 = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+
+        expected2 = [
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 1, 0],
+            [1, 0, 1, 0],
+        ]
+
+        expected3 = [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0],
+        ]
+
+        self.assertListEqual(
+            bin_time1_mat,
+            expected1
+        )
+
+        self.assertListEqual(
+            bin_time2_mat,
+            expected2
+        )
+
+        self.assertListEqual(
+            bin_time3_mat,
+            expected3
+        )
+
+
+class TestDisplay(unittest.TestCase):
+    def test(self):
+        display = Display()
+        bin_time = BinaryTime(12, 1, 13)
+
+        bin_str = display.bin_time_to_str(bin_time)
+
+        expected_bin_str = \
+            "[ ][ ][ ][ ][ ][ ]\n" \
+            "[ ][ ][ ][ ][ ][ ]\n" \
+            "[ ][X][ ][ ][ ][X]\n" \
+            "[X][ ][ ][X][X][X]\n"
 
         self.assertEqual(
-            bin_time1.hour_ones,
-            [0, 0, 0, 0]
+            bin_str,
+            expected_bin_str
         )
 
 if __name__ == '__main__':
